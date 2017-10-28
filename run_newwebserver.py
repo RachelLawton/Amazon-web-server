e2dcddc4f498eecc65f9df28918499b8bd475dae
Rachel Lawton <rachel.m.d.lawton@gmail.com>
2017-10-28 20:10:30 +0100

    ssh added

--- run_newwebserver.py
+++ run_newwebserver.py
@@ -25,24 +25,67 @@ def create_instances():
     print ("Instance is being created")
     print ("*************************")
     time.sleep(5)
-    print ("Succes an instance with ID", instances[0].id, "has been created.")
+    print ("Success an instance with ID", instances[0].id, "has been created.")
+    print ("*************************")
+    print ("Getting DNS name from instance")
+    print ("*************************")
+    time.sleep(4)
+    instances[0].reload()
+    print("Dns name is: ", instances[0].public_dns_name)
     print ("*************************")
 
-def checknginx():
-    cmd = 'ps -A | grep nginx | grep -v grep'
 
-    (status, output) = subprocess.getstatusoutput(cmd)
-
-    if status > 0:  
-        print("Nginx Server IS NOT running")
+    print ("Please wait while we ssh into the new instance")
+    print ("*************************")
+    cmd_ssh = ("ssh -t -o StrictHostKeyChecking=no -i rachel_lawton_dev.pem ec2-user@" + instances[0].public_dns_name + " 'sudo pwd'")
+    time.sleep(60)
+    (status, output) = subprocess.getstatusoutput(cmd_ssh)
+    print ("status", status)
+    print ("output" + output)
+    if status > 0:
+        print ("ssh to instance was not successful")
     else:
-        print("Nginx Server IS running")
+        print ("ssh to instancewas successful")
+
+   
+
+
+
+# def copingCWS(public_dns_name):
+
+    
+#     print ("Copying check_webserver file to new instance created using scp command")
+#     print ("*************************")
+#     cmd_scp = ("scp -i /Users/rachellawton/Amazon-web-server/rachel_lawton_dev.pem check_webserver.py ec2-user@" + public_dns_name +  ":.")
+#     time.sleep(20)
+#     (status, output) = subprocess.getstatusoutput(cmd_scp)
+#     print ("status", status)
+#     print ("output" + output)
+#     if status > 0:
+#         print ("Copy to instance was not successful")
+#     else:
+#         print ("Copy to instancewas successful")
+
+
+
+
+# def checknginx():
+#     cmd = 'ps -A | grep nginx | grep -v grep'
+
+#     (status, output) = subprocess.getstatusoutput(cmd)
+
+#     if status > 0:  
+#         print("Nginx Server IS NOT running")
+#     else:
+#         print("Nginx Server IS running")
 
 
 
 def main():
     create_instances()
-    checknginx()
+    #checknginx()
+    #sshIntoInstance(instances)
+
 if __name__ == '__main__':
   main()
 
