ff02dc7a7f5ffe1d57abc0806db65a7e1efc4ceb
Rachel Lawton <rachel.m.d.lawton@gmail.com>
2017-10-30 15:30:03 +0000

    functionalities added
    scp copies check_webserver.py to instance
    using ssh command
    executes it using chmod
    but docent print anything
    bucket created

--- run_newwebserver.py
+++ run_newwebserver.py
@@ -8,6 +8,8 @@ import subprocess
 ec2 = boto3.resource('ec2')
 s3 = boto3.resource('s3')
 
+
+
 def create_instances():
 
     instances = ec2.create_instances(
@@ -21,7 +23,6 @@ def create_instances():
             service nginx start
             chkconfig nginx on''', 
         InstanceType='t2.micro')
-
     print ("Instance is being created")
     print ("*************************")
     time.sleep(5)
@@ -29,15 +30,19 @@ def create_instances():
     print ("*************************")
     print ("Getting DNS name from instance")
     print ("*************************")
+    instances = instances[0]
     time.sleep(4)
-    instances[0].reload()
-    print("Dns name is: ", instances[0].public_dns_name)
+    instances.reload()
+    print("Dns name is: ", instances.public_dns_name)
     print ("*************************")
 
+
+    # global dns_name
+    # dns_name = instances[0].public_dns_name
 
     print ("Please wait while we ssh into the new instance")
     print ("*************************")
-    cmd_ssh = ("ssh -t -o StrictHostKeyChecking=no -i rachel_lawton_dev.pem ec2-user@" + instances[0].public_dns_name + " 'sudo pwd'")
+    cmd_ssh = ("ssh -t -o StrictHostKeyChecking=no -i rachel_lawton_dev.pem ec2-user@" + instances.public_dns_name + " 'sudo pwd'")
     time.sleep(60)
     (status, output) = subprocess.getstatusoutput(cmd_ssh)
     print ("status", status)
@@ -47,44 +52,75 @@ def create_instances():
     else:
         print ("ssh to instancewas successful")
 
-   
+    #return dns_name
 
 
 
-# def copingCWS(public_dns_name):
-
-    
-#     print ("Copying check_webserver file to new instance created using scp command")
-#     print ("*************************")
-#     cmd_scp = ("scp -i /Users/rachellawton/Amazon-web-server/rachel_lawton_dev.pem check_webserver.py ec2-user@" + public_dns_name +  ":.")
-#     time.sleep(20)
-#     (status, output) = subprocess.getstatusoutput(cmd_scp)
-#     print ("status", status)
-#     print ("output" + output)
-#     if status > 0:
-#         print ("Copy to instance was not successful")
-#     else:
-#         print ("Copy to instancewas successful")
-
+#def copyingCWS(dns_name):
+    print ("Copying check_webserver file to new instance created using scp command")
+    print ("*************************")
+    cmd_scp = ("scp -i /Users/rachellawton/Amazon-web-server/rachel_lawton_dev.pem /Users/rachellawton/Amazon-web-server/check_webserver.py ec2-user@" + instances.public_dns_name +  ":.")
+    time.sleep(20)
+    #instances = instances[0]
+    (status, output) = subprocess.getstatusoutput(cmd_scp)
+    print ("status", status)
+    print ("output" + output)
+    if status > 0:
+        print ("Copy to instance was not successful")
+    else:
+        print ("Copy to instance was successful")
 
 
+    print ("*************************")
+    print ("Making the file executable usng the chmod command")
+    cmd_chmod = "ssh –i /Users/rachellawton/Amazon-web-server/rachel_lawton_dev.pem ec2-user@" + instances.public_dns_name + " 'chmod +x check_webserver.py'"
+    # (status, output) = subprocess.getstatusoutput(cmd_chmod)
+    # if status == 0:
+    #     print ("File execution was not successful")
+    # else:
+    #     print ("File execution was successful")
+    print ("*************************")
+    print ("running the executed file ")
+    cmd_runfile = "ssh –i /Users/rachellawton/Amazon-web-server/rachel_lawton_dev.pem ec2-user@" + instances.public_dns_name + " ./check_webserver.py"
+    # (status, output) = subprocess.getstatusoutput(cmd_runfile)
+    # if status == 0:
+    #     print ("File was not successful")
+    # else:
+    #     print ("File was successful")
 
-# def checknginx():
-#     cmd = 'ps -A | grep nginx | grep -v grep'
+def checknginx():
+    cmd = 'ps -A | grep nginx | grep -v grep'
 
-#     (status, output) = subprocess.getstatusoutput(cmd)
+    (status, output) = subprocess.getstatusoutput(cmd)
 
-#     if status > 0:  
-#         print("Nginx Server IS NOT running")
-#     else:
-#         print("Nginx Server IS running")
+    if status > 0:  
+        print("Nginx Server IS NOT running")
+    else:
+        print("Nginx Server IS running")
+
+
+
+def create_bucket():
+    print("Creating bucket please wait")
+    print ("*************************")
+    bucket_name = input("Please name your bucket:")
+    try:
+        response = s3.create_bucket(Bucket=bucket_name,CreateBucketConfiguration={'LocationConstraint': 'us-east-2'})
+        print ("bucket was created")
+        print (response)
+    except Exception as error:
+        print (error)
+        
+    return response
 
 
 
 def main():
     create_instances()
-    #checknginx()
-    #sshIntoInstance(instances)
+    #copyingCWS(dns_name)
+    checknginx()
+    create_bucket()
+    
 
 if __name__ == '__main__':
   main()
